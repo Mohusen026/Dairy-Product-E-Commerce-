74245dea638e855a507b4697466d4dccb33f420f
Mo_husen <mhusenhaji0@gmail.com>
2024-07-25 15:22:44 +0530

    second commit

--- app/views.py
+++ app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from urllib import request
+from django.http import HttpResponse
+from django.views import View
 
 
 # Create your views here.
 def home(request):
     return render(request,"app/home.html")
+
+class CategoryView(View): #if you are using the class so you are pass the parameter as a viewe
+     
+    
